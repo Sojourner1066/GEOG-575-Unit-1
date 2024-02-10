f55c383d9cca2b5cc3fe094771d9fb0d3944c7a2
Sojourner1066 <morgangrundy@gmail.com>
2024-02-10 17:46:39 +0000

    Working on Activity 6

---
+++ Unit-2/Avtivity 6/js/adaptedTutorial.js
@@ -0,0 +1,53 @@
+/* Map of GeoJSON data from MegaCities.geojson */
+//declare map var in global scope
+var map;
+//function to instantiate the Leaflet map
+function createMap(){
+    //create the map
+    map = L.map('map', {
+        center: [20, 0],
+        zoom: 2
+    });
+
+    //add OSM base tilelayer
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+    }).addTo(map);
+
+    //call getData function
+    getData();
+};
+
+//function to retrieve the data and place it on the map
+function getData(){
+    //load the data
+    fetch("data/MegaCities.geojson")
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){            
+            //create marker options
+            var geojsonMarkerOptions = {
+                radius: 8,
+                fillColor: "#ff7800",
+                color: "#000",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+            };
+            //create a Leaflet GeoJSON layer and add it to the map
+            L.geoJson(json, {
+                pointToLayer: function (feature, latlng){
+                    return L.circleMarker(latlng, geojsonMarkerOptions);
+                }
+            }).addTo(map);
+        });
+        /*.then(function(json){
+            //create a Leaflet GeoJSON layer and add it to the map
+            L.geoJson(json).addTo(map);
+        }) 
+
+        */
+};
+
+document.addEventListener('DOMContentLoaded',createMap)
