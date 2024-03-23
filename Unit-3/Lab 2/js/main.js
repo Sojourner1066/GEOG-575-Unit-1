9b7a6a85bc8c531a73703288bd2ee5acb30d1caf
Sojourner1066 <morgangrundy@gmail.com>
2024-03-23 15:22:54 +0100

    Activity 8 Complete

--- Unit-3/Lab 2/js/main.js
+++ Unit-3/Lab 2/js/main.js
@@ -5,7 +5,7 @@ window.onload = function(){
     //SVG dimension variables
     var w = 900, h = 500;
 
-    //Example 1.2 line 1...container block
+    //container block
     var container = d3.select("body") //get the <body> element from the DOM
        .append("svg") //put a new svg in the body
        .attr("width", w) //assign the width
@@ -64,13 +64,46 @@ window.onload = function(){
         }
     ];
 
-        //above Example 2.8 line 20
+    //find the minimum value of the array
+    var minPop = d3.min(cityPop, function(d){
+        return d.population;
+    });
+
+    //find the maximum value of the array
+    var maxPop = d3.max(cityPop, function(d){
+        return d.population;
+    });
+
+    //scale for circles center y coordinate
+    // var y = d3.scaleLinear()
+    //     .range([440, 95])
+    //     .domain([
+    //         minPop,
+    //         maxPop
+    //     ]);
+
+    var y = d3.scaleLinear()
+        .range([450, 50]) //was 440, 95
+        .domain([0, 700000]); //was minPop, maxPop
+
+    //color scale generator 
+    var color = d3.scaleLinear()
+        .range([
+            "#FDBE85",
+            "#D94701"
+        ])
+        .domain([
+            minPop, 
+            maxPop
+        ]);
+
+
     var x = d3.scaleLinear() //create the scale
-        .range([90, 810]) //output min and max
-        .domain([0, 3]); //input min and max
+        .range([90, 950]) //output min and max
+        .domain([0, 4]); //input min and max
     console.log(x)
 
-    //Example 2.6 line 3
+
     var circles = container.selectAll(".circles") //create an empty selection
         .data(cityPop) //here we feed in an array
         .enter() //one of the great mysteries of the universe
@@ -94,7 +127,65 @@ window.onload = function(){
         })
         .attr("cy", function(d){
             //subtract value from 450 to "grow" circles up from the bottom instead of down from the top of the SVG
-            return 450 - (d.population * 0.0005);
+            //return 450 - (d.population * 0.0005);
+            return y(d.population);
+        })
+        .style("fill", function(d, i){ //add a fill based on the color scale generator
+            return color(d.population);
+        })
+        .style("stroke", "#000"); //black circle stroke
+
+        //Example 3.6 line 1...create y axis generator
+    var yAxis = d3.axisLeft(y);
+
+        //create axis g element and add axis
+    var axis = container.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(50, 0)")
+        .call(yAxis);
+
+    // Add Title    
+    var title = container.append("text")
+        .attr("class", "title")
+        .attr("text-anchor", "middle")
+        .attr("x", 450)
+        .attr("y", 30)
+        .text("City Populations");
+
+    var labels = container.selectAll(".labels")
+        .data(cityPop)
+        .enter()
+        .append("text")
+        .attr("class", "labels")
+        .attr("text-anchor", "left")
+        .attr("y", function(d){
+            //vertical position centered on each circle
+            return y(d.population) -2;
         });
 
+    //first line of label
+    var nameLine = labels.append("tspan")
+        .attr("class", "nameLine")
+        .attr("x", function(d,i){
+            //horizontal position to the right of each circle
+            return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
+        })
+        .text(function(d){
+            return d.city;
+        });
+    
+        //create format generator
+    var format = d3.format(",");
+
+    //second line of label
+    var popLine = labels.append("tspan")
+        .attr("class", "popLine")
+        .attr("x", function(d,i){
+            //horizontal position to the right of each circle
+            return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
+        })
+        .attr("dy", "15") 
+        .text(function(d){
+            return "Pop. " + format(d.population);;
+        });
 };
