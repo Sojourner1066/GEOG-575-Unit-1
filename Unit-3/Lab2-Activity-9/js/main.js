51b3f6a7e9a319e4e0548f9f266fdab71f900eb2
Sojourner1066 <morgangrundy@gmail.com>
2024-03-24 15:08:40 +0100

    got the geojson to display

--- Unit-3/Lab2-Activity-9/js/main.js
+++ Unit-3/Lab2-Activity-9/js/main.js
@@ -5,9 +5,39 @@ window.onload = setMap();
 
 //set up choropleth map
 function setMap(){
+    //map frame dimensions
+    var width = 960,
+        height = 460;
+
+    //create new svg container for the map
+    var map = d3.select("body")
+        .append("svg")
+        .attr("class", "map")
+        .attr("width", width)
+        .attr("height", height);
+
+    //create Albers equal area conic projection centered on France
+    // var projection = d3.geoAlbers()
+    //     .center([0, 46.2])
+    //     .rotate([-2, 0, 0])
+    //     .parallels([43, 62])
+    //     .scale(2500)
+    //     .translate([width / 2, height / 2]);
+
+    var projection = d3.geoAlbers()
+        .center([0, 9.96])
+        .rotate([-8.09, 0.00, 0])
+        .parallels([8.18, 32.22])
+        .scale(2000)
+        .translate([width / 2, height / 2]);
+
+    var path = d3.geoPath()
+        .projection(projection);
+
+         
     //use Promise.all to parallelize asynchronous data loading
     var promises = [d3.csv("data/NGA_Poverty_Data.csv"),                    
-                    d3.json("data/NGA_Regions.topojson"),                    
+                    d3.json("data/NGA_Region.topojson"),                    
                     d3.json("data/NGA_States.topojson")                   
                     ];    
     Promise.all(promises).then(callback);
@@ -16,8 +46,36 @@ function callback(data) {
     var csvData = data[0],
         region = data[1],
         states = data[2];
+    
+    var regionCountries = topojson.feature(region, region.objects.NGA_Region),
+        nigerianStates = topojson.feature(states, states.objects.NGA_States);
+    
+
+    //add African countries to map
+    var countries = map.append("path")
+        .datum(regionCountries)
+        .attr("class", "countries")
+        .attr("d", path);
+
+    // testing states data
+    // var nga_states = map.append("path")
+    //     .datum(nigerianStates)
+    //     .attr("class", "states")
+    //     .attr("d", path);
+
+    //add Nigerian regions to map
+    var regions = map.selectAll(".regions")
+        .data(nigerianStates.features)
+        .enter()
+        .append("path")
+        .attr("class", function(d){
+            return "regions " + d.properties.State_Name;
+        })
+        .attr("d", path);
+
     console.log(csvData);
-    console.log(region);
-    console.log(states);
+    console.log(regionCountries);
+    console.log(nigerianStates);
+
     };
 };
