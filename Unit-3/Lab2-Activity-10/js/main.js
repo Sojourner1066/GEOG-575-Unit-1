5d5d0c388453d6c009d3cebef426b483db26f741
Sojourner1066 <morgangrundy@gmail.com>
2024-03-28 14:46:35 +0000

    Activity 10

--- Unit-3/Lab2-Activity-10/js/main.js
+++ Unit-3/Lab2-Activity-10/js/main.js
@@ -7,6 +7,12 @@
     //pseudo-global variables
     var attrArray = ["varA", "varB", "varC", "varD", "varE"]; //list of attributes
     var expressed = attrArray[0]; //initial attribute
+    var title_dict = {"varA":"Percent of population vulnerable to poverty", 
+                         "varB":"Percent of population in severe poverty", 
+                         "varC":"Number of poor people by region (in thousands)", 
+                         "varD":"Number of vulnerable by region (in thousands)", 
+                         "varE":"Number of people in severe poverty by region (in thousands)"
+    }
     
     //begin script when window loads
     window.onload = setMap();
@@ -140,11 +146,16 @@
     // Quantiles
     function makeColorScale(data){
         var colorClasses = [
-            "#D4B9DA",
-            "#C994C7",
-            "#DF65B0",
-            "#DD1C77",
-            "#980043"
+            "#f2f0f7",
+            "#cbc9e2",
+            "#9e9ac8",
+            "#756bb1",
+            "#54278f",
+            // "#D4B9DA",
+            // "#C994C7",
+            // "#DF65B0",
+            // "#DD1C77",
+            // "#980043"
         ];
     
         //create color scale generator
@@ -174,6 +185,70 @@
             .attr("width", chartWidth)
             .attr("height", chartHeight)
             .attr("class", "chart");
+
+         //create a scale to size bars proportionally to frame
+        var yScale = d3.scaleLinear()
+            .range([0, chartHeight])
+            .domain([0, 105]);
+            
+        //set bars for each province
+        var bars = chart.selectAll(".bars")
+            .data(csvData)
+            .enter()
+            .append("rect")
+            .sort(function(a, b){
+                return b[expressed]-a[expressed]
+            })
+            .attr("class", function(d){
+                return "bars " + d.Region;
+            })
+            .attr("width", chartWidth / csvData.length - 1)
+            .attr("x", function(d, i){
+                return i * (chartWidth / csvData.length);
+            })
+            // .attr("height", 460)
+            // .attr("y", 0);
+            
+            .attr("height", function(d){
+                return yScale(parseFloat(d[expressed]));
+            })
+            .attr("y", function(d){
+                return chartHeight - yScale(parseFloat(d[expressed]));
+            })
+                    //Example 2.5 line 23...end of bars block
+            .style("fill", function(d){
+                return colorScale(d[expressed]);
+            });
+            //annotate bars with attribute value text
+        var numbers = chart.selectAll(".numbers")
+        .data(csvData)
+        .enter()
+        .append("text")
+        .sort(function(a, b){
+            return b[expressed]-a[expressed]
+        })
+        .attr("class", function(d){
+            return "numbers " + d.Region;
+        })
+        .attr("text-anchor", "middle")
+        .attr("x", function(d, i){
+            var fraction = chartWidth / csvData.length;
+            return i * fraction + (fraction - 1) / 2;
+        })
+        .attr("y", function(d){
+            return chartHeight - yScale(parseFloat(d[expressed])) + 15;
+        })
+        .text(function(d){
+            return Math.round(d[expressed]);
+        });
+
+        //below Example 2.8...create a text element for the chart title
+        var chartTitle = chart.append("text")
+            .attr("x", 20)
+            .attr("y", 40)
+            .attr("class", "chartTitle")
+            .text(title_dict[expressed]);
+
     };
     //Natural Breaks
     //function to create color scale generator
